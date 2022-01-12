20a5f42a33fbfacb0402b478627c578b789881d8
AbubakarShf <ItsAbubakarShf@gmail.com>
2022-01-12 22:32:55 +0500

    auth-and-cssFixes-done

--- FaceRecogApp/views.py
+++ FaceRecogApp/views.py
@@ -1,38 +1,59 @@
-from django.http import response
 from django.shortcuts import render
-from rest_framework.views import APIView
-from .serializer import RegisterSerializer
-from rest_framework.response import Response
-from rest_framework import status
 from .models import User
 from django.shortcuts import render, redirect
-from rest_framework.exceptions import AuthenticationFailed
 from django.urls import reverse
-# from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from django.db import IntegrityError
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 
 def login_user(request):
     if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request, email=email, password=password)
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect(reverse("CameraFormPage"))
         else:
-            messages.success(request, "Email or Password is wrong, Try Again!")
-            return render(request, 'index.html', {{messages}})
+            messages.success(
+                request, ("Email or Password is wrong, Try Again!"))
+            return render(request, 'index.html', {})
     else:
         return render(request, 'index.html')
 
 
 def register_user(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("CameraFormPage"))
+    else:
+        return render(request, "register.html")
 
 
 @login_required(login_url='HomePage')
